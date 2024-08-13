aabd5c92d657c505589294213db3d7a91be76d76
HyunSeung, Shin <tlsgustm12@gmail.com>
2024-08-13 16:16:32 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import User from "../lib/Class/User";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,44 +14,20 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  /** getUser Example START */
-  // let idx: number = 1;
-  // const user = await User.getUser(idx);
-  /** getUser Example END */
-
-  /** getUsers Example START */
-  // let wheres = [
-  //   "u.idx in (1,2)"
-  // ];
-  // let orderby = [
-  //   "u.idx DESC"
-  // ]
-  // let limit = [
-  //   "0, 10"
-  // ]
-  // const users = await User.getUsers(wheres, orderby, limit);
-  /** getUsers Example END */
-
-  /** setUser UPDATE Example START  */
-  // const params = {
-  //   idx: 1,
-  //   username: "test"
-  // };
-  // const setUserUpdate = await User.setUser(params);
-  /** setUser UPDATE Example END  */
-
-  /** setUser INSERT Example START  */
-  // const params = {
-  //   username: "신현승"
-  // };
-  // const setUserInsert = await User.setUser(params);
-  /** setUser INSERT Example END  */
   
-  // console.log(users);
   return (
     
     <html lang="ko">
-      <body>{children}</body>
+      
+      <body>
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+      
+
     </html>
   );
 }
